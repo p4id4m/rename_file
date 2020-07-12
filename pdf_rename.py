16936fbc514124db533034ab49a24502c336ed2a
p4id4m <47162147+p4id4m@users.noreply.github.com>
2020-07-12 16:05:41 -0400

    Update pdf_rename.py

--- pdf_rename.py
+++ pdf_rename.py
@@ -5,14 +5,14 @@ from bs4 import BeautifulSoup
 
 url = "url to crawl here"
 
-#If there is no such folder, the script will create one automatically
+# Look for folder and if folder doesn't exist, create it
 folder_location = r'C:/Users/p4ill/OneDrive/Documents/DnD'
 if not os.path.exists(folder_location):os.mkdir(folder_location)
 
 response = requests.get(url)
 soup= BeautifulSoup(response.text, "html.parser")     
 for link in soup.select("a[href$='.pdf']"):
-    #Name the pdf files using the last portion of each link which are unique in this case
+    # Name pdf files based on the last part of the link supplied
     filename = os.path.join(folder_location,link['href'].split('/')[-1])
     with open(filename, 'wb') as f:
         f.write(requests.get(urljoin(url,link['href'])).content)
