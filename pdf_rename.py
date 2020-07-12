312bed0321193e1b48121e4acf80549739d6214d
p4id4m <47162147+p4id4m@users.noreply.github.com>
2020-07-12 15:36:21 -0400

    Update pdf_rename.py

--- pdf_rename.py
+++ pdf_rename.py
@@ -1,4 +1,23 @@
-folder = 'file path here'
+import os
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+url = "url to crawl here"
+
+#If there is no such folder, the script will create one automatically
+folder_location = r'C:/Users/p4ill/OneDrive/Documents/DnD'
+if not os.path.exists(folder_location):os.mkdir(folder_location)
+
+response = requests.get(url)
+soup= BeautifulSoup(response.text, "html.parser")     
+for link in soup.select("a[href$='.pdf']"):
+    #Name the pdf files using the last portion of each link which are unique in this case
+    filename = os.path.join(folder_location,link['href'].split('/')[-1])
+    with open(filename, 'wb') as f:
+        f.write(requests.get(urljoin(url,link['href'])).content)
+
+folder = 'folder path for files to be renamed'
 import os
 pathiter = (os.path.join(root, filename)
     for root, _, filenames in os.walk(folder)
