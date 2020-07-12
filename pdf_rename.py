65635d288e30c83dc925649d119e2becd7a96b63
p4id4m <47162147+p4id4m@users.noreply.github.com>
2020-07-12 15:19:25 -0400

    Add files via upload

---
+++ pdf_rename.py
@@ -0,0 +1,16 @@
+folder = 'C:/Users/p4ill/OneDrive/Documents/DnD'
+import os
+pathiter = (os.path.join(root, filename)
+    for root, _, filenames in os.walk(folder)
+    for filename in filenames
+)
+for path in pathiter:
+    newname =  path.replace('%20', ' ')
+    newname1 =  path.replace('%27', ' ')
+    newname2 =  path.replace('%5', ' ')
+    if newname != path:
+        os.rename(path,newname)
+    if newname1 != path:
+        os.rename(path,newname1)
+    if newname2 != path:
+        os.rename(path,newname2)
